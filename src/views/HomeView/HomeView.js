2e6564ffd8a5af6cbc35a24b350d9c641b2158c0
lockelee <q280835887@qq.com>
2016-03-13 13:39:01 +0800

    first commit

---
+++ src/views/HomeView/HomeView.js
@@ -0,0 +1,14 @@
+/* @flow */
+import React from 'react'
+
+export class HomeView extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Joinfor Docker</h1>
+      </div>
+    )
+  }
+}
+
+export default HomeView
